5860af28addd82896a48df364083c988961df058
Anik <sikder35-1408@diu.edu.bd>
2022-02-26 16:09:04 +0600

    hide and display the buttons

--- app.js
+++ app.js
@@ -19,13 +19,30 @@ prevBtn.addEventListener('click', function(){
 });
 
 function carousel(){
-    if(counter === slides.length){
-        counter = 0;
+    // if(counter === slides.length){
+    //     counter = 0;
+    // }
+    // if(counter < 0){
+    //     counter = slides.length -1;
+    // }
+    //prevBtn display
+    if(counter > 0){
+        prevBtn.style.display = 'block';
+    }else{
+        prevBtn.style.display = 'none';
     }
-    if(counter < 0){
-        counter = slides.length -1;
+    //nextBtn display
+    if(counter < slides.length -1){
+        nextBtn.style.display = 'block';
+    }else{
+        nextBtn.style.display = 'none';
     }
+
+
     slides.forEach(function(slide){
         slide.style.transform = `translateX(-${counter * 100}%)`;
     })
 }
+
+//hide prevBt initially
+prevBtn.style.display = 'none';
